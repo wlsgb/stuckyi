06529ee00770b6f45fb694790a2777d045f3b26e
wlsgb94 <wlsgb94@naver.com>
2022-01-17 16:22:25 +0900

    project start

---
+++ src/main/java/com/stuckyi/StuckyiApplication.java
@@ -0,0 +1,13 @@
+package com.stuckyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuckyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuckyiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stuckyi/StuckyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuckyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuckyiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
